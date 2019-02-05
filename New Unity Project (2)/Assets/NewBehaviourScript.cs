be02aff83ba8374c189dd016fd7ea33705fb72e6
wei <qq60956092@gmail.com>
2019-02-05 11:15:18 -0600

    lab3

--- New Unity Project (2)/Assets/NewBehaviourScript.cs
+++ New Unity Project (2)/Assets/NewBehaviourScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NewBehaviourScript : MonoBehaviour
 {
    
@@ -15,29 +16,47 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.LeftArrow))
+        move();
+    }
+
+
+void move()
+{
+        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.x, 0);
+        Cursor.lockState = CursorLockMode.Locked;
+    if (Input.GetKeyDown(KeyCode.LeftArrow))
+    {
+        Vector3 position = this.transform.position;
+        position.z++;
+        this.transform.position = position;
+    }
+    if (Input.GetKeyDown(KeyCode.RightArrow))
+    {
+        Vector3 position = this.transform.position;
+        position.z--;
+        this.transform.position = position;
+    }
+    if (Input.GetKeyDown(KeyCode.UpArrow))
+    {
+        Vector3 position = this.transform.position;
+        position.x++;
+        this.transform.position = position;
+    }
+    if (Input.GetKeyDown(KeyCode.DownArrow))
+    {
+        Vector3 position = this.transform.position;
+        position.x--;
+        this.transform.position = position;
+    }
+
+        float h = Input.GetAxis("Mouse X");
+        float v = Input.GetAxis("Mouse Y");
+        transform.Rotate(-v, 0, 0);
+        transform.Rotate(0,h, 0);
+        if (this.transform.rotation.eulerAngles.z != 0)
         {
-            Vector3 position=this.transform.position;
-            position.z++;
-            this.transform.position = position;
+            this.transform.Rotate(0, 0, -this.transform.rotation.eulerAngles.z);
         }
-        if (Input.GetKeyDown(KeyCode.RightArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.z--;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.UpArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x++;
-            this.transform.position = position;
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
-        {
-            Vector3 position = this.transform.position;
-            position.x--;
-            this.transform.position = position;
-        }
+
     }
 }
