f973bc3b89bd582ff3f48d49d0c391505ac4d631
Wei <qq60956092@gmail.com>
2019-03-12 10:10:07 -0600

    3/11

--- New Unity Project (2)/Assets/NewBehaviourScript.cs
+++ New Unity Project (2)/Assets/NewBehaviourScript.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class NewBehaviourScript : MonoBehaviour
 {
    
@@ -16,47 +15,29 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move();
-    }
-
-
-void move()
-{
-        transform.rotation = Quaternion.Euler(0, transform.eulerAngles.x, 0);
-        Cursor.lockState = CursorLockMode.Locked;
-    if (Input.GetKeyDown(KeyCode.LeftArrow))
-    {
-        Vector3 position = this.transform.position;
-        position.z++;
-        this.transform.position = position;
-    }
-    if (Input.GetKeyDown(KeyCode.RightArrow))
-    {
-        Vector3 position = this.transform.position;
-        position.z--;
-        this.transform.position = position;
-    }
-    if (Input.GetKeyDown(KeyCode.UpArrow))
-    {
-        Vector3 position = this.transform.position;
-        position.x++;
-        this.transform.position = position;
-    }
-    if (Input.GetKeyDown(KeyCode.DownArrow))
-    {
-        Vector3 position = this.transform.position;
-        position.x--;
-        this.transform.position = position;
-    }
-
-        float h = Input.GetAxis("Mouse X");
-        float v = Input.GetAxis("Mouse Y");
-        transform.Rotate(-v, 0, 0);
-        transform.Rotate(0,h, 0);
-        if (this.transform.rotation.eulerAngles.z != 0)
+        if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            this.transform.Rotate(0, 0, -this.transform.rotation.eulerAngles.z);
+            Vector3 position=this.transform.position;
+            position.z++;
+            this.transform.position = position;
         }
-
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.z--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
     }
 }
---
+++ New Unity Project (2)/Assets/npc.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npc : MonoBehaviour
+{
+    public int intel = 5;
+    public bool hit_on_head = true;
+    float test()
+    {
+        return 5.0f;
+    }
+    void Greet()
+    {
+        switch (intel)
+        {
+            case 5:
+                print("Hello, good sir!Do you like physics");
+                //if (hit_on_head == true)
+                //{ print("Get shot on the head");
+                //}
+                //intel = 4;
+                //Greet();
+
+                break;
+            case 4:
+                print("Ello guv!");
+                break;
+            case 3:
+                print("what you want?");
+                break;
+            case 2:
+                print("ug...me hunger");
+                break;
+            case 1:
+                print("Grrrrrrrrr....*fart*");
+                break;
+            case 0:
+                print("drools");
+                break;
+            default:
+                print("hello!");
+                break;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Greet();
+        float test_num = test();
+        print(test_num);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (intel > 0)
+            {
+                intel--;
+            }
+            Greet();
+            print("get hit on the head");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (intel < 5)
+            { 
+            intel++;
+        }
+            Greet();
+            print("read a book");
+        }
+
+    }
+}
